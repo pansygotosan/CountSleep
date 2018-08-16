bb3a1f8cf1be254b28865653d590a4972a2311bd
pansygotosan <ayaka.gotou.ayaka.gotou@gmail.com>
2018-08-16 16:04:05 +0900

    〜〜〜〜

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :target_sleeptime])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username, :target_sleeptime])
     end
+    def after_sign_in_path_for(resource_or_scope)
+        sleeps_index_path
+     end
+
+     def after_sign_out_path_for(resource_or_scope)
+         new_user_session
+     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,23 @@ Rails.application.routes.draw do
   get 'sleeps/new'
   get 'sleeps/user'
   get 'sleeps/setgoal'
+  get 'sessions/new'
   devise_for :users
   resources :sleeps
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/"=>"sleeps#index"
+
+devise_scope :user do
+       get '/users/sign_out' => 'devise/sessions#destroy'
+     end
+   authenticated :users do
+        root :to => "sleeps#index"
+    end
+
+    unauthenticated :users do
+        root :to => "sessions#new"
+    end
+
+
 end
